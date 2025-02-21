44ed86afadfcbae9c3e0f764afb2b3743dcf1e24
GeorgiLazov <georgilazov04@gmail.com>
2025-02-21 00:22:10 -0800

    Delete Raster_Graphics/RasterGraphics directory

--- Raster_Graphics/RasterGraphics/Commands/Specific/AddMonochrome.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../Manage.h"
-#include "../Transformations/Specific/Monochrome.h"
-class AddMonochrome : public Manage
-{
-public:
-	AddMonochrome(ListOfSessions*& list);
-	void execute() override;
-};
